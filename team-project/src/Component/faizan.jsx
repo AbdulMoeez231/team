0c9589851757fadaca1f18aca52b179515978269
AbdulMoeez231 <moeezbuttab231@gmail.com>
2023-06-17 14:30:45 +0500

    changes

--- team-project/src/Component/faizan.jsx
+++ team-project/src/Component/faizan.jsx
@@ -1,163 +1,160 @@
 import React from 'react'
 import video1 from '../Component/vidd.mp4'
 const faizan = () => {
-  return (
-    <div>
-      <div className='grid lg:grid-cols-6 md:grid-cols-3 ms:grid-cols-1 place-items-center	'>
-      <div className='w-[100%]  h-[370px] border-black'>
+      return (
+            <>
+                  <div className='py-5'>
+                        <div className='grid lg:grid-cols-6 md:grid-cols-3 ms:grid-cols-1 place-items-center'>
+                              <div className='w-[100%]  h-[370px] border-black'>
 
-<div className='w-[100%] h-[200px] '>
-
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
-
-        
-      </div>
-
-      <div className='w-[100%]  h-[370px] border-black'>
-
-<div className='w-[100%] h-[200px] '>
-
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
-
-        
-      </div>
+                                    <div className='w-[100%] h-[200px] '>
+                                          <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                    </div>
+                                    <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                    <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                    <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
 
 
-      <div className='w-[100%]  h-[370px] border-black'>
+                              </div>
 
-<div className='w-[100%] h-[200px] '>
+                              <div className='w-[100%]  h-[370px] border-black'>
 
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
+                                    <div className='w-[100%] h-[200px] '>
 
-        
-      </div>
-
+                                          <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                    </div>
+                                    <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                    <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                    <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
 
 
-      <div className='w-[100%]  h-[370px] border-black'>
-
-<div className='w-[100%] h-[200px] '>
-
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
-
-        
-      </div>
+                              </div>
 
 
-      <div className='w-[100%]  h-[370px] border-black'>
+                              <div className='w-[100%]  h-[370px] border-black'>
 
-<div className='w-[100%] h-[200px] '>
+                                    <div className='w-[100%] h-[200px] '>
 
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
+                                          <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                    </div>
+                                    <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                    <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                    <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
 
-        
-      </div>
+
+                              </div>
 
 
 
+                              <div className='w-[100%]  h-[370px] border-black'>
 
-      <div className='w-[100%]  h-[370px] border-black'>
+                                    <div className='w-[100%] h-[200px] '>
 
-<div className='w-[100%] h-[200px] '>
+                                          <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                    </div>
+                                    <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                    <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                    <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
 
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
 
-        
-      </div>
+                              </div>
+
+
+                              <div className='w-[100%]  h-[370px] border-black'>
+
+                                    <div className='w-[100%] h-[200px] '>
+
+                                          <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                    </div>
+                                    <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                    <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                    <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
+
+
+                              </div>
 
 
 
 
+                              <div className='w-[100%]  h-[370px] border-black'>
+
+                                    <div className='w-[100%] h-[200px] '>
+
+                                          <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                    </div>
+                                    <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                    <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                    <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
 
 
-      </div>
-
-      <div className='w-[100%]     mt-2 grid  md:grid-cols-1 lg:grid-cols-2 sm:grid-cols-1 '>
-
-<div className=' md:w-[100%] h-[max-content]   border-black grid grid-cols-1 gap-2 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1'>
-
-
-<div className='w-[100%]  h-[370px] border-black'>
-
-<div className='w-[100%] h-[200px] '>
-
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
-
-        
-      </div>
-      <div className='w-[100%]  h-[370px] border-black'>
-
-<div className='w-[100%] h-[200px] '>
-
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
-
-        
-      </div>
-      <div className='w-[100%]  h-[370px] border-black'>
-
-<div className='w-[100%] h-[200px] '>
-
-    <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
-</div>
-<div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
-<div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
-<button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
-
-        
-      </div>
-      
-
-</div>
+                              </div>
 
 
 
 
 
 
+                        </div>
+
+                        <div className='w-[100%]     mt-2 grid  md:grid-cols-1 lg:grid-cols-2 sm:grid-cols-1 '>
+
+                              <div className=' md:w-[100%] h-[max-content]   border-black grid grid-cols-1 gap-2 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1'>
 
 
-<div className=' h-[max-content]  border-black grid'>
-<video controls  src={video1}></video>
+                                    <div className='w-[100%]  h-[370px] border-black'>
 
-</div>
+                                          <div className='w-[100%] h-[200px] '>
+
+                                                <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                          </div>
+                                          <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                          <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                          <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
 
 
-      </div>
-      
-    </div>
-  )
+                                    </div>
+                                    <div className='w-[100%]  h-[370px] border-black'>
+
+                                          <div className='w-[100%] h-[200px] '>
+
+                                                <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                          </div>
+                                          <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                          <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                          <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
+
+
+                                    </div>
+                                    <div className='w-[100%]  h-[370px] border-black'>
+
+                                          <div className='w-[100%] h-[200px] '>
+
+                                                <img className='w-[100%] h-[200px] relative' src="https://i5.walmartimages.com/asr/248e3fe0-f182-43bf-be67-6fd75eeabb70.97866c48de952c947f6f2101caaa6435.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" alt="" />
+                                          </div>
+                                          <div className='w-[150px] h-[30px] mt-3 font-bold p-2'>From$21.99</div>
+                                          <div className='w-[100%] h-[60px]  mt-3 text-sm p-2'>VILINICE Bluetooth Speakers Portable Wireless</div>
+                                          <button className='w-[100px] h-[30px] border border-black  rounded-full mt-2  btn-hover ml-2'>options</button>
+
+
+                                    </div>
+
+
+                              </div>
+
+
+                              <div className=' h-[max-content] grid'>
+                                    <video autoPlay loop src={video1}></video>
+
+                              </div>
+
+
+                        </div>
+
+                  </div>
+
+            </>
+
+      )
 }
 
 export default faizan
